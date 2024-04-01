e7f2ae27c67ef9d734f169c53cdf9d223c8970a6
emilyyr55 <emilyyr55@gmail.com>
2024-04-01 08:42:40 -0400

    finished commit

--- app_api/models/blogs.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require( 'mongoose' );
-
-var blogSchema = new mongoose.Schema({
-    blogTitle: {type: String, required: true},
-    blogText: String,
-    createdOn: {type: Date, default: Date.now}
-});
-
-mongoose.model('Blog', blogSchema);
